dce8528b727125543f481021766e6be1880a9b8a
RafalUklejewski <rafaluklejewski@gmail.com>
2017-11-13 23:03:30 +0000

    OK Start

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -6,11 +6,10 @@ public class FirstScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("First Change");
-        print("First Change");
     }
 	
 	// Update is called once per frame
 	void Update () {
-       
+        print("And we have liftoff!!");
     }
 }
