0f841865919447c746d0d8b5035975aaa2b4ebcf
RafalUklejewski <rafaluklejewski@gmail.com>
2017-11-13 23:42:02 +0000

    Number Wizard Functions

--- Assets/Number Wizard/NumberWizard.cs
+++ Assets/Number Wizard/NumberWizard.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 
+    int min = 1;
+    int max = 1000;
+    int startGuess;
+    int guess;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        max +=1 ;
+        startGuess = max / 2;
+
+        print("Welcome to Number Wizard");
+        print("Pick a number in your head, but don't tell me!");
+        print("The highest number you can pick is " +max);
+        print("The lowest number you can pick is " + min);
+        print("Pick your number now!");
+        print(" ");
+        print(" ");
+        print(" ");
+        print("Is your number higher or lower than" + startGuess + " ?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guess = (max + min) / 2;
+            print("Higher or lower than " + guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (max + min) / 2;
+            print("Higher or lower than " + guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("I won");
+        }
+    }
 }
