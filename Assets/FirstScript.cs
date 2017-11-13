228ac4040c4ac869dbf9612ec23f4fa08e5c9855
RafalUklejewski <rafaluklejewski@gmail.com>
2017-11-13 22:57:14 +0000

    Working

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("First Change");
+        print("First Change");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+}
