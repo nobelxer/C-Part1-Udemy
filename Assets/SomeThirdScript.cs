23ebe4a9bac2e5cdf7841cb16518971474858091
RafalUklejewski <rafaluklejewski@gmail.com>
2017-11-13 23:05:09 +0000

    New One

---
+++ Assets/SomeThirdScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeThirdScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
